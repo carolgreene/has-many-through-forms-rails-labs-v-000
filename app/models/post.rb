73a3cf21c3b2a4bc148d0933228a11cd38bb19c1
Carol Greene <carolgreene3@gmail.com>
2018-06-08 21:33:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,12 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-  def categories_attributes=(categories_hashes)
-    categories_hashes.each do |i, category_attributes|
-      if category_attributes[:name].present?
-        category = Category.find_or_create_by(name: category_attributes[:name])
-        if !self.categories.include?(category)
-          self.post_categories.build(:category => category)
+  def categories_attributes=(categories_hashes)                              #need this for the new categories which
+    categories_hashes.each do |i, category_attributes|                         #becomes a hash when you use field_for
+      if category_attributes[:name].present?                                      #the activerecord category= won't work
+        category = Category.find_or_create_by(name: category_attributes[:name])    #It's set for an array
+        if !self.categories.include?(category)                                      #This hash acts like an array bc the key is
+          self.post_categories.build(:category => category)                         #basically the index
         end
       end
     end
